1586f88d65dbb2aede4c53ba7305e67efd3f723c
takuabonn <k.takumi19980322@gmail.com>
2023-04-23 13:29:28 +0900

    タスク一覧のテストコード 作成

--- server_test.go
+++ server_test.go
@@ -44,7 +44,7 @@ func TestServer_Run(t *testing.T) {
 		t.Fatalf("failed to read body: %v", err)
 	}
 
-	want := fmt.Sprintf("Hello %s!", in)
+	want := fmt.Sprintf("Hello, %s!", in)
 	if string(got) != want {
 		t.Errorf("want %q, but got %q", want, got)
 	}
