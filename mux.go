21b57c965e207c3c8b09e0cb8220261a488451bd
takuabonn <k.takumi19980322@gmail.com>
2023-04-30 12:39:52 +0900

    user登録

--- mux.go
+++ mux.go
@@ -24,15 +24,24 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 	if err != nil {
 		return nil, cleanup, err
 	}
+
 	r := store.Repository{Clocker: clock.RealClocker{}}
 	at := &handler.AddTask{
 		Service:   &service.AddTask{DB: db, Repo: &r},
 		Validator: v,
 	}
 	mux.Post("/tasks", at.ServeHTTP)
+
 	lt := &handler.ListTask{
 		Service: &service.ListTask{DB: db, Repo: &r},
 	}
 	mux.Get("/tasks", lt.ServeHTTP)
+
+	ru := &handler.RegisterUser{
+		Service:   &service.RegisterUser{DB: db, Repo: &r},
+		Validator: v,
+	}
+	mux.Post("/register", ru.ServeHTTP)
+
 	return mux, cleanup, nil
 }
