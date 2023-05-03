3109cbc8486cb334ea7cd095ba1b698bc420240d
takuabonn <k.takumi19980322@gmail.com>
2023-05-03 19:13:19 +0900

    user_idをタスクテーブルに追加及びエンドポイントを認証認可で保護

--- mux.go
+++ mux.go
@@ -27,16 +27,25 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 	}
 	clocker := clock.RealClocker{}
 	r := store.Repository{Clocker: clocker}
+
+	rcli, err := store.NewKVS(ctx, cfg)
+	jwter, err := auth.NewJWTer(rcli, clocker)
+	if err != nil {
+		return nil, cleanup, err
+	}
+
 	at := &handler.AddTask{
 		Service:   &service.AddTask{DB: db, Repo: &r},
 		Validator: v,
 	}
-	mux.Post("/tasks", at.ServeHTTP)
-
 	lt := &handler.ListTask{
 		Service: &service.ListTask{DB: db, Repo: &r},
 	}
-	mux.Get("/tasks", lt.ServeHTTP)
+	mux.Route("/tasks", func(r chi.Router) {
+		r.Use(handler.AuthMiddleware(jwter))
+		r.Get("/", lt.ServeHTTP)
+		r.Post("/", at.ServeHTTP)
+	})
 
 	ru := &handler.RegisterUser{
 		Service:   &service.RegisterUser{DB: db, Repo: &r},
@@ -44,16 +53,19 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 	}
 	mux.Post("/register", ru.ServeHTTP)
 
-	rcli, err := store.NewKVS(ctx, cfg)
-	jwter, err := auth.NewJWTer(rcli, clocker)
-	if err != nil {
-		return nil, cleanup, err
-	}
 	li := &handler.Login{
 		Service:   &service.Login{DB: db, Repo: &r, TokenGenerator: jwter},
 		Validator: v,
 	}
 	mux.Post("/login", li.ServeHTTP)
+
+	mux.Route("/admin", func(r chi.Router) {
+		r.Use(handler.AuthMiddleware(jwter), handler.AdminMiddleware)
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			_, _ = w.Write([]byte(`{"message": "admin only"}`))
+		})
+	})
 
 	return mux, cleanup, nil
 }
--- store/task.go
+++ store/task.go
@@ -7,14 +7,14 @@ import (
 )
 
 func (r *Repository) ListTasks(
-	ctx context.Context, db Queryer,
+	ctx context.Context, db Queryer, id entity.UserID,
 ) (entity.Tasks, error) {
 	tasks := entity.Tasks{}
 	sql := `SELECT
-			id, title,
+			id, user_id, title,
 			status, created, modified
-		FROM task;`
-	if err := db.SelectContext(ctx, &tasks, sql); err != nil {
+		FROM task WHERE user_id = ?;`
+	if err := db.SelectContext(ctx, &tasks, sql, id); err != nil {
 		return nil, err
 	}
 	return tasks, nil
@@ -29,10 +29,10 @@ func (r *Repository) AddTask(
 	t.Modified = r.Clocker.Now()
 
 	sql := `INSERT INTO task
-		(title, status, created, modified)
-	VALUES (?, ?, ?, ?)`
+		(user_id, title, status, created, modified)
+	VALUES (?, ?, ?, ?, ?)`
 
-	result, err := db.ExecContext(ctx, sql, t.Title, t.Status, t.Created, t.Modified)
+	result, err := db.ExecContext(ctx, sql, t.UserID, t.Title, t.Status, t.Created, t.Modified)
 
 	if err != nil {
 		return err
